d2789dfbb1c866a5fc5f6a212c03d9c054d105b6
Rob Hilgefort <rjhilgefort@gmail.com>
2018-06-25 14:05:14 -0600

    derp

--- src/server/orderbook.ts
+++ src/server/orderbook.ts
@@ -1,21 +1,17 @@
 import { Request, Response } from 'express';
-import * as R from 'ramda';
 import { log } from '../lib/logger';
 import fetchOrderbook from '../orderbook';
 import { CombindedOrderbook } from '../types/exchanges';
-import * as U from '../utils';
 
 export const GET = (req: Request, res: Response) => {
   const market = req.query.market;
   log(`==> Fetching market: ${market}`);
-  return R.compose(
-    U.thenP2(
-      (orderbook: CombindedOrderbook) =>
-        res.status(200).send({ data: orderbook }))
-      (() =>
-       res.status(200).send({
-         error: `Could not process market on all exchanges: ${market}`,
-       })),
-    fetchOrderbook,
-  )({ market });
+  return fetchOrderbook({ market })
+    .then((orderbook: CombindedOrderbook) => {
+      res.status(200).send({ data: orderbook })
+    }, () => {
+      res.status(200).send({
+        error: `Could not process market on all exchanges: ${market}`,
+      });
+    });
 };
